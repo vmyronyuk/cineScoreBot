474cc3cc04f91008ca6a7d5ef7a52726d6ed5901
Vitaliy Myroniuk <vitalikmur38@gmail.com>
2025-08-25 22:19:34 +0300

    return random movie with poster and description

--- src/bot.ts
+++ src/bot.ts
@@ -2,15 +2,18 @@ import dotenv from 'dotenv'
 import { Telegraf } from 'telegraf'
 import { registerRateAction } from './commands/addRating/actions/registerRateAction'
 import { addRatingCommandHandler } from './commands/addRating/addRatingCommandHandler'
+import { randomCommandHandler } from './commands/random/randomCommandHandler'
 
 dotenv.config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN!)
-const tmdbKey = process.env.TMDB_API_KEY
+export const TMDB_API_KEY = process.env.TMDB_API_KEY!
 
 bot.start(ctx => ctx.reply('Hello!'))
 
 bot.command('addRating', addRatingCommandHandler)
 registerRateAction(bot)
+
+bot.command('random', randomCommandHandler)
 
 bot.launch()
---
+++ src/commands/random/randomCommandHandler.ts
@@ -0,0 +1,60 @@
+import { Context, Markup } from 'telegraf'
+import { TMDB_API_KEY } from '../../bot'
+import { TMDBMovie } from '../../types/movies'
+import { replyHTML } from '../../utils/replyHTML'
+
+export async function randomCommandHandler(ctx: Context) {
+	try {
+		const randomPage = Math.floor(Math.random() * 500) + 1
+
+		const url = new URL('https://api.themoviedb.org/3/discover/movie')
+		url.search = new URLSearchParams({
+			api_key: TMDB_API_KEY,
+			page: String(randomPage),
+			language: 'uk-UA',
+			sort_by: 'popularity.desc',
+			'vote_average.gte': '6',
+			'vote_count.gte': '100',
+		}).toString()
+
+		const res = await fetch(url.toString())
+
+		const tmdbData = await res.json()
+
+		if (!tmdbData.results || tmdbData.results.length === 0) {
+			return await replyHTML(ctx, 'Фільм не знайдено')
+		}
+
+		const randomMovie = tmdbData.results[
+			Math.floor(Math.random() * tmdbData.results.length)
+		] as TMDBMovie
+
+		const movieTitle = randomMovie.title
+		const movieOverview = randomMovie.overview
+		const posterUrl = randomMovie.poster_path
+			? `https://image.tmdb.org/t/p/w500${randomMovie.poster_path}`
+			: null
+
+		const message = `<b>${movieTitle}</b>\n\n${movieOverview}`
+
+		if (posterUrl) {
+			await ctx.replyWithPhoto(posterUrl, {
+				caption: message,
+				parse_mode: 'HTML',
+				reply_markup: Markup.inlineKeyboard([
+					Markup.button.switchToChat('🔗 Поділитись', movieTitle),
+				]).reply_markup,
+			})
+		} else {
+			await ctx.reply(message, {
+				parse_mode: 'HTML',
+				reply_markup: Markup.inlineKeyboard([
+					Markup.button.switchToChat('🔗 Поділитись', movieTitle),
+				]).reply_markup,
+			})
+		}
+	} catch (error) {
+		console.error(error)
+		ctx.reply('Сталася помилка при отриманні випадкового фільму ⚠️ ')
+	}
+}
