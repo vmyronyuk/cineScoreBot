9ec6b9e8e7b0024de50e16206065e81aa3f98c6a
Vitaliy Myroniuk <vitalikmur38@gmail.com>
2025-08-28 22:25:48 +0300

    add base stats command

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -18,3 +18,32 @@ export async function createUserIfNotExist(
 
 	return user as User
 }
+
+export async function getUserStats(userId: number) {
+	const { data, error } = await supabase
+		.from('user_ratings')
+		.select(`userId, rating, movies(runtime)`)
+		.eq('userId', userId)
+
+	if (error || !data) {
+		return {
+			numberOfRatings: 0,
+			totalRuntime: 0,
+		}
+	}
+
+	const numberOfRatings = data.length
+	const totalWatchTime = data.reduce(
+		(sum, r: any) => sum + (r.movies?.runtime ?? 0),
+		0
+	)
+	const avgRating = (
+		data.reduce((sum, r: any) => sum + r.rating, 0) / numberOfRatings
+	).toFixed(1)
+
+	return {
+		numberOfRatings,
+		totalWatchTime,
+		avgRating,
+	}
+}
