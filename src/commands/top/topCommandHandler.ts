3ea0597b8609e3e61a50f9279abdc9954ad738d1
Vitaliy Myroniuk <vitalikmur38@gmail.com>
2025-09-13 20:18:21 +0300

    add mytop and top command

--- src/commands/top/topCommandHandler.ts
+++ src/commands/top/topCommandHandler.ts
@@ -1,23 +1,51 @@
 import { Context } from 'telegraf'
-import { getUserMovies } from '../../services/userService'
+import { getUsersMovies } from '../../services/usersService'
 import { replyHTML } from '../../utils/replyHTML'
 
 export async function topCommandHandler(ctx: Context) {
-	const userId = ctx.from?.id!
-
 	try {
-		const userMovies = await getUserMovies(userId)
+		const usersMovies = await getUsersMovies()
 
-		if (!userMovies.length) {
-			return ctx.reply('😕 У тебе ще немає оцінених фільмів')
+		if (!usersMovies.length) {
+			return ctx.reply('😕 Не знайдено топ фільмів')
 		}
 
-		const topMovies = userMovies.sort((a, b) => b.rating - a.rating).slice(0, 5)
+		const moviesMap = new Map<string, { title: string; ratings: number[] }>()
+
+		for (const entry of usersMovies) {
+			const id = entry.movie.id
+			if (!moviesMap.has(id)) {
+				moviesMap.set(id, { title: entry.movie.title, ratings: [entry.rating] })
+			} else {
+				moviesMap.get(id)!.ratings.push(entry.rating)
+			}
+		}
+
+		const moviesStats = Array.from(moviesMap.values()).map(m => {
+			const avgRating = m.ratings.reduce((a, b) => a + b, 0) / m.ratings.length
+			return {
+				title: m.title,
+				avgRating,
+				count: m.ratings.length,
+			}
+		})
+
+		const topMovies = moviesStats
+			.sort((a, b) => b.avgRating - a.avgRating)
+			.slice(0, 5)
+
 		await replyHTML(
 			ctx,
 			'🎬 <strong>Топ-5 фільмів</strong>:\n\n' +
 				topMovies
-					.map((m, i) => `${i + 1}. ${m.movie.title} - ${m.rating}`)
+					.map(
+						(m, i) =>
+							`${i + 1}. <strong>${m.title}</strong> - ${m.avgRating.toFixed(
+								1
+							)} ⭐️ (${m.count} оцін${
+								m.count === 1 ? 'ка' : m.count < 5 ? 'ки' : 'ок'
+							})`
+					)
 					.join('\n')
 		)
 	} catch (error) {
