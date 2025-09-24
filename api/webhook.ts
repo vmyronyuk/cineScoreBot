ebd597c2bbd49fa299d0b7ae33458f3e94d6342f
Vitaliy Myroniuk <vitalikmur38@gmail.com>
2025-09-24 20:44:20 +0300

    update settings

--- api/webhook.ts
+++ api/webhook.ts
@@ -2,14 +2,11 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import { bot } from '../src/bot'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-	if (req.method === 'POST') {
-		try {
-			await bot.handleUpdate(req.body, res)
-		} catch (error) {
-			console.error('Bot handler error', error)
-			res.status(500).send('Bot handler error')
-		}
-	} else {
-		res.status(200).send('Bot is live')
+	try {
+		await bot.handleUpdate(req.body, res)
+		res.status(200).send('Bot handler success')
+	} catch (error) {
+		console.error('Bot handler error', error)
+		res.status(500).send('Bot handler error')
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import { bot } from './bot'
 if (!process.env.VERCEL) {
 	bot.launch()
 	console.log('🤖 Bot is running locally with long polling')
+} else {
+	console.log('🤖 Bot running in webhook mode on Vercel')
 }
